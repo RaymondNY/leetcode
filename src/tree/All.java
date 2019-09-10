1a79e01ba1a09bdd9e427e6d92658872f2328d80
zqx <317171584@qq.com>
2019-09-10 15:41:27 +0800

    tree

--- src/tree/All.java
+++ src/tree/All.java
@@ -5,10 +5,16 @@ import java.util.LinkedList;
 
 public class All {
     public static void main(String[] args) {
+//        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(new Integer[]{
+//                3,2,9,null,null,10,null,null,8,null,4
+//        }));
+//        TreeNode treeNode = BinaryTree.creatPreBinaryTree(list);
+//        Traversal.preOrderTraversal(treeNode);
+
         LinkedList<Integer> list = new LinkedList<>(Arrays.asList(new Integer[]{
-                3,2,9,null,null,10,null,null,8,null,4
+                null,1,null,2,null,3,null,4,5,6,7,8,9,null
         }));
-        TreeNode treeNode = BinaryTree.creatPreBinaryTree(list);
-        Traversal.preOrderTraversal(treeNode);
+        TreeNode treeNode = BinaryTree.creatInBinaryTree(list);
+        Traversal.inOrderTraversal(treeNode);
     }
 }
