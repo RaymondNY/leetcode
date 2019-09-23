dec06b266e912b8eb87a2f6c6efa20270017f098
zqx <317171584@qq.com>
2019-09-23 17:31:49 +0800

    tree

--- src/sort/Quicksort.java
+++ src/sort/Quicksort.java
@@ -1,13 +1,43 @@
 package sort;
 
-public class Quicksort {
-    public void quick(int[] a){
-        int ref = a[0];
-        int right=a.length-1;
-        int left = 0;
-        if (left >= right) {
+import java.util.Arrays;
 
+public class Quicksort {
+    public static void quick(int[] arr,int start ,int last){
+        if (start == last) {
+            return ;
         }
-        return ;
+        //找基准位置
+        int pivotIndex = partition(arr,start,last);
+
+        quick(arr,start,pivotIndex-1);
+        quick(arr,pivotIndex+1,last);
+    }
+    private static int partition(int[] arr, int start, int last) {
+        int left = start;
+        int right = last;
+        int pivot = arr[start];
+        while (left!=right){
+            while (left<right &&arr[right]>=pivot){
+                --right;
+            }
+            while (left<right &&arr[left]<=pivot){
+                ++left;
+            }
+            if (left<right) {
+                int p = arr[left];
+                arr[left]=arr[right];
+                arr[right]=p;
+            }
+        }
+        arr[start]=arr[left];
+        arr[left]=pivot;
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,8,5,6};
+        quick(a,0,a.length-1);
+        System.out.println(Arrays.toString(a));
     }
 }
--- src/tree/All.java
+++ src/tree/All.java
@@ -22,19 +22,18 @@ public class All {
 //        }));
 //        TreeNode treeNode = BinaryTree.creatPreBinaryTree(list);
 //        Traversal.inOrderTraversal(treeNode);
-        TraceThreadPool traceThreadPool = new TraceThreadPool(5,10,0, TimeUnit.NANOSECONDS,new ArrayBlockingQueue<Runnable>(10));
-        for (int i = 0; i < 5; i++) {
-            int finalI = i;
-            traceThreadPool.execute(new Runnable() {
-                @Override
-                public void run() {
-                    double a = 5/ finalI;
-                    System.out.println(a+Thread.currentThread().getName());
-                }
-            });
-        }
-        traceThreadPool.shutdown();
-    }
+//        TraceThreadPool traceThreadPool = new TraceThreadPool(5,10,0, TimeUnit.NANOSECONDS,new ArrayBlockingQueue<Runnable>(10));
+//        for (int i = 0; i < 5; i++) {
+//            int finalI = i;
+//            traceThreadPool.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    double a = 5/ finalI;
+//                    System.out.println(a+Thread.currentThread().getName());
+//                }
+//            });
+//        }
+//        traceThreadPool.shutdown();
 //        ExecutorService executorService = Executors.newFixedThreadPool(5);
 //        for (int i = 0; i < 5; i++) {
 //            int finalI = i;
@@ -48,4 +47,9 @@ public class All {
 //        }
 //        executorService.shutdown();
 //    }
+        ThreadLocal<Integer> local = new ThreadLocal<>();
+        local.set(1);
+        local.set(2);
+        System.out.println(local.get());
+    }
 }
