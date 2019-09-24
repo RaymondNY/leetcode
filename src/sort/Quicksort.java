57fc1499f46d9bd1f0f2eeaa1e1d16e014a1474e
zqx <317171584@qq.com>
2019-09-24 10:57:12 +0800

    tree

--- src/sort/Quicksort.java
+++ src/sort/Quicksort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Quicksort {
     public static void quick(int[] arr,int start ,int last){
-        if (start == last) {
+        if (start >= last) {
             return ;
         }
         //找基准位置
@@ -13,28 +13,45 @@ public class Quicksort {
         quick(arr,start,pivotIndex-1);
         quick(arr,pivotIndex+1,last);
     }
+//    //双边循环
+//    private static int partition(int[] arr, int start, int last) {
+//        int left = start;
+//        int right = last;
+//        int pivot = arr[start];
+//        while (left!=right){
+//            while (left<right &&arr[right]>=pivot){
+//                --right;
+//            }
+//            while (left<right &&arr[left]<=pivot){
+//                ++left;
+//            }
+//            if (left<right) {
+//                int p = arr[left];
+//                arr[left]=arr[right];
+//                arr[right]=p;
+//            }
+//        }
+//        arr[start]=arr[left];
+//        arr[left]=pivot;
+//        return left;
+//    }
+    //单边循环
     private static int partition(int[] arr, int start, int last) {
-        int left = start;
-        int right = last;
+        int mark = start;
         int pivot = arr[start];
-        while (left!=right){
-            while (left<right &&arr[right]>=pivot){
-                --right;
-            }
-            while (left<right &&arr[left]<=pivot){
-                ++left;
-            }
-            if (left<right) {
-                int p = arr[left];
-                arr[left]=arr[right];
-                arr[right]=p;
+        for (int i = start+1; i <= last; i++) {
+            if (arr[i] < pivot) {
+                mark++;
+                int tem = arr[i];
+                arr[i] = arr[mark];
+                arr[mark]=tem;
             }
         }
-        arr[start]=arr[left];
-        arr[left]=pivot;
-        return left;
-    }
+        arr[start]=arr[mark];
+        arr[mark] = pivot;
+        return mark;
 
+    }
     public static void main(String[] args) {
         int[] a = {1,8,5,6};
         quick(a,0,a.length-1);
